6305c04f7b144a540e1f6c3074b5165cbb60bf32
SennaOmphe <90115638+SennaOmphe@users.noreply.github.com>
2022-09-27 11:16:14 +0200

    additional commit

---
+++ ConnectedOffice/DeviceManagement_WebApp/Repository/DeviceRepository.cs
@@ -0,0 +1,18 @@
+﻿using DeviceManagement_WebApp.Data;
+using DeviceManagement_WebApp.Models;
+
+namespace DeviceManagement_WebApp.Repository
+{
+    public class DeviceRepository : GenericRepository<Device>, IDeviceRepository
+    {
+        public DeviceRepository(ConnectedOfficeContext context) : base(context)
+        {
+
+        }
+
+        public Device GetMostRecentDevice()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ ConnectedOffice/DeviceManagement_WebApp/Repository/ZoneRepository.cs
@@ -0,0 +1,18 @@
+﻿using DeviceManagement_WebApp.Data;
+using DeviceManagement_WebApp.Models;
+
+namespace DeviceManagement_WebApp.Repository
+{
+    public class ZoneRepository : GenericRepository<Zone>, IZoneRepository
+    {
+        public ZoneRepository(ConnectedOfficeContext context) : base(context)
+        {
+
+        }
+
+        public Zone GetMostRecentZone()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
